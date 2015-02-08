fe632fd72e4a8d3221dd4b920cc610ba98800eda
::Qry3v@vna~* <qry3vavna@gmail.com>
2015-02-08 01:45:30 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 # install Qry3vavna scripts
 
 main() {
@@ -27,6 +27,7 @@ parse_args() {
 
 the_action() {
   bashrc_check
+  vimrc_check
   for tool in ${list[*]};do
     if [[ "$ACTION" == "install" ]];then
       cp ./$tool /usr/local/bin/
@@ -34,10 +35,10 @@ the_action() {
     else
       rm -f /usr/local/bin/$tool
     fi
-  done 
+  done
 }
 
-bashrc_check(){
+bashrc_check() {
 # Check if scripts aliases are already mentioned,
 # if not add them, if so remove them
   if [[ $(grep "Qry3vavna\|debianplus" ~/.bashrc) ]];then # it exists, remove it
@@ -52,6 +53,42 @@ bashrc_check(){
       echo -e "\n# Qry3vavna: Collection of scripts and aliases to type less and do more.">>~/.bashrc
       echo -e "[[ -e ~/.debianplus ]] && . ~/.debianplus">>~/.bashrc
       . ~/.bashrc # reload bash
+    fi
+  fi
+}
+
+vimrc_check() {
+# Check for vimrc and replace if necessary
+  if [[ -e ~/.vimrc ]];then # it already exists
+    if [[ $(grep -i qry3 ~/.vimrc) ]];then # we're there
+      if [[ "$(sha1sum ~/.vimrc|cut -d' ' -f1)" != "79a0470d7e2e4aa696446bc312294bb45ff3b867" ]];then # not our v0.1 copy :(
+        if [[ "$ACTION" == "install" ]];then
+          cp ~/.vimrc{,.script} # Archive current
+          cp ./.vimrc ~/.vimrc
+          chmod 600 ~/.vimrc
+        else # uninstall
+          if [[ -e ~/.vimrc.script ]];then
+            cp ~/.vimrc{.script,} # Reset archive
+          else # Try to remove each line... still dirty and leaves stuff behind :(
+            while IFS='\n' read line;do
+              eval "sed -i '/$line/d' ~/.vimrc"
+            done< <(sed -e '/^$/d;s/\*.*//;s/\/.*//;s/"//;s/<.*//;s/".*//' .vimrc)
+          fi
+        fi
+      else # it's only our copy, whack it
+        if [[ "$ACTION" == "uninstall" ]];then
+          rm -f ~/.vimrc
+        fi
+      fi
+    else # exists, we're not in there
+      if [[ "$ACTION" == "install" ]];then
+        cat ./.vimrc >> ~/.vimrc
+      fi
+    fi
+  else # doesn't even exist
+    if [[ "$ACTION" == "install" ]];then
+      cp ./.vimrc ~/.vimrc
+      chmod 600 ~/.vimrc
     fi
   fi
 }
