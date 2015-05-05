fc1157c69a2d3b04eff1dd8c19e636a0264a9205
Qry3v@vna~* <qry3vavna@gmail.com>
2015-05-05 13:19:43 -0400

    New man page, updated install

--- install.sh
+++ install.sh
@@ -10,10 +10,7 @@ main() {
 initialize() {
   version="0.1 install"
   list=(ctime hs pacounts poports rports)
-<<<<<<< HEAD
   manf=(rports.man)
-=======
->>>>>>> 254f13e99a9bf3bbdd3a169e6cf23a4fb1369ad6
   ACTION=
 }
 
